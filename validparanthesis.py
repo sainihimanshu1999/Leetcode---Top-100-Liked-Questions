c6f386973f5afe303e784cdd9d5c3e65150df00d
Himanshu Saini <sainihimanshu.1999@gmail.com>
2021-03-05 13:48:07 +0530

    leetcode practice

---
+++ validparanthesis.py
@@ -0,0 +1,29 @@
+def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = [] 
+        for char in s: 
+            if char in ["(", "{", "["]: 
+  
+                stack.append(char) 
+            else: 
+   
+                if not stack: 
+                    return False
+                current_char = stack.pop() 
+                if current_char == '(': 
+                    if char != ")": 
+                        return False
+                if current_char == '{': 
+                    if char != "}": 
+                        return False
+                if current_char == '[': 
+                    if char != "]": 
+                        return False
+  
+     
+        if stack: 
+            return False
+        return True
